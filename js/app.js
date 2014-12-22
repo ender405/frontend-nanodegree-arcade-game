a34ba7a11b63289668c92e2e6be4f1f408c9f510
ender405 <laslack@gmail.com>
2014-12-21 21:41:33 -0700

    Initial commit, next thing is enemy movement and collision

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +16,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    this.x = this.x + (Math.floor(Math.random() * 6) + 1) * dt;
+    this.y = this.y;
 }
 
 // Draw the enemy on the screen, required method for game
@@ -25,12 +30,51 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x,y) {
+    this.sprite = 'images/char-boy.png'
+    this.x = x;
+    this.y = y;
+
+}
+
+Player.prototype.update = function(dt) {
+
+    this.x = this.x;
+    this.y = this.y;
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+}
+
+Player.prototype.handleInput = function(key) {
+
+    if (key === 'left') {
+        this.x = this.x - 101;
+    };
+
+    if (key === 'right') {
+        this.x = this.x + 101;
+    };
+
+    if (key === 'up') {
+        this.y = this.y - 83;
+    };
+
+    if (key === 'down') {
+        this.y = this.y + 83;
+    };
+
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [new Enemy(0,1*101), new Enemy(0,2*101), new Enemy(0,3*101)];
 
-
+var player = new Player(2*101, 4*101);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
